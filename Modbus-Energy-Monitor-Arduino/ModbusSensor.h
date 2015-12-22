31fffe8bdbb0a476f63259c14b8a30c7a686dac9
Jaime García <peninquen@users.noreply.github.com>
2015-12-22 15:11:29 +0100

    New version, no-blocking
    New version tested, no-blocking during transmisions

--- Modbus-Energy-Monitor-Arduino/ModbusSensor.h
+++ Modbus-Energy-Monitor-Arduino/ModbusSensor.h
@@ -3,7 +3,7 @@
   create ModbusSensor and ModbusMaster classes to process values from
   a Eastron SMD120 and family.
 
-  version 0.3 BETA 20/12/2015
+  version 0.3 BETA 22/12/2015
 
   Author: Jaime García  @peninquen
   License: Apache License Version 2.0.
@@ -20,7 +20,8 @@
 // We can reduce it to maximum data sending by a slave SMD120 9 bytes, SMD 630 is 85 bytes
 // Three phase meters are 3 values, 6 registers and 12 bytes, plus 5 frame bytes, total 17
 #define BUFFER_SIZE      32
-#define WAITING_INTERVAL 30
+#define TIMEOUT          110  // time to fail a request 
+#define WAITING_INTERVAL 40   // time required by SDM120 to be prepared to receive a new request
 
 // What happens when _status is diferent to MB_VALID_DATA?
 #define CHANGE_TO_ZERO 0x00
@@ -31,7 +32,7 @@ class modbusMaster;
 
 union dataFloat {
   float f;
-  uint8_t arr[4];
+  uint8_t array[4];
 };
 
 //------------------------------------------------------------------------------
@@ -39,6 +40,9 @@ class modbusSensor {
   public:
     // Constructor
     modbusSensor(modbusMaster *mbm, uint8_t id, uint16_t adr, uint8_t hold);
+
+    // Constructor
+    //    modbusSensor(uint8_t id, uint16_t adr, uint8_t hold);
 
     // read value in defined units
     float read();
@@ -69,15 +73,18 @@ class modbusSensor {
 class modbusMaster {
   public:
     //constructor
-    modbusMaster(HardwareSerial *MBSerial, uint8_t TxEnPin);
+    modbusMaster(HardwareSerial *mbSerial, uint8_t TxEnPin);
 
-    // Connect modbusSensor to modbusMaster array of queries
-    void connect(modbusSensor *mbs);
+    // Connect a modbusSensor to modbusMaster array of queries
+    void connect(modbusSensor *mbSensor);
+
+    // Disconnect a modbusSensor to modbusMaster array of queries
+    void disconnect(modbusSensor *mbSensor);
 
     // begin communication using ModBus protocol over RS485
-    void begin(uint16_t baudrate, uint8_t byteFormat, uint16_t timeOut, uint16_t pollInterval);
+    void begin(uint16_t baudrate, uint8_t byteFormat, uint16_t pollInterval);
 
-    // end coummunication over serial port
+    // end communication over serial port
     void end();
 
     // Finite State Machine core, process FSM and check if the array of sensors has been requested
@@ -89,16 +96,16 @@ class modbusMaster {
     uint8_t  _state;                    // Modbus FSM status (SENDING, RECEIVING, STANDBY, WAINTING_NEXT_POLL)
     uint8_t  _TxEnablePin;              // pin to enable transmision in MAX485
     uint8_t  _totalSensors;             // constant, max number of sensors to poll
-    uint16_t _timeOut;                  // constant, time since lastMillis to fail poll
     uint16_t _pollInterval;             // constant, time between polling same data
-//    uint16_t _T1_5;                     // inter-character time in microseconds
+    uint32_t _T2_5;                     // time between characters in a frame, in microseconds
     uint8_t  _buffer[BUFFER_SIZE];      // buffer to process rececived frame
-    uint8_t  _availableSensors;         // number of refreshed sensors, decrement when read, increment when write
-    uint16_t _availableSensorsFlag;     // array of flags to register new available sensor values
+    //    uint8_t  _availableSensors;         // number of refreshed sensors, decrement when read, increment when write
+    //    uint16_t _availableSensorsFlag;     // array of flags to register new available sensor values
     HardwareSerial  *_MBSerial;
     modbusSensor    *_mbSensorsPtr[MAX_SENSORS]; // array of modbusSensor's pointers
     modbusSensor    *_mbSensorPtr;
     uint8_t         *_framePtr;
 };
+extern modbusMaster MBSerial;
 
 #endif
