a8241a007fc012bde3380321b5f479f0cc5e8a3d
Jaime García <jgarciapons@gmail.com>
2015-12-22 15:25:31 +0100

    commit changes

--- Modbus-Energy-Monitor-Arduino/Modbus-Energy-Monitor-Arduino.ino
+++ Modbus-Energy-Monitor-Arduino/Modbus-Energy-Monitor-Arduino.ino
@@ -1,39 +1,7 @@
 /**********************************************************************
-* ModbusEnergyMonitor example
-* An example to collect data from a Modbus energy monitor using ModbusSensor class
-* to datalogger, include a RTC DS3231 and a SD card
-* version 0.3 ALPHA 21/09/2015
-* 
-* Author: Jaime García  @peninquen
-* License: Apache License Version 2.0.
-*
 **********************************************************************/
 /*
-PULSE ELECTRICAL ENERGY MONITOR
-DDS238-1 SINGLE-PHASE DIN-RAIL TYPE WATT-HOUR METER
-TECHNICAL DETAILS
-STANDARD IEC 62053-21 (IEC61036)
-Nominal voltage: 120/220/230/240 V +-10%
-Basic current (It): 5 A
-Maximun current (Ib1) 32 A
-Minimun current: 0.02 A
-Frequency: 50/60 Hz
-Consumption: <2w / 10 VA
-Accurancy class: 1
-Display: Mechanical 5+1 digits
-         LCD 5+1 / 6+1 digits
-Interface: Open colector output (SO)  SO+ ---------------------- PIN 2 (INT0) INTERNAL PULLUP RESISTOR
-           18-27V 27mA                SO- ---------------------- GND
-Impulse length: >= 30ms
-units conversion:
-Energy: 3200 imp = 1 KWh -> 1 imp = 0,3125 Wh;
-Power: 1 imps/s = 1125 W;
-Current(voltage 230V)-> 1 imp/s = 4.89 A;
-maximum impulse frequency (30 A): 6.13 imp/s -> period 163 ms
-basic frequency (5 A): 1.02 imp/s -> period 978.3 ms
-minimun impulse frequency (0.02 A): 1.0 imp/min-> period 60000 ms
-Mounting: DIN rail 18 mm (1 module)
-include flasing led proportional to load (1000 imp/KWh)
+
 */
 #define SERIAL_OUTPUT 1
 
@@ -49,28 +17,68 @@ include flasing led proportional to load (1000 imp/KWh)
 
 #include "ModbusSensor.h"
 
-#define SERIAL_PORT 0 // Arduino only one serial port, Mega has 3 serial ports.
-#define ID_SLAVE 1
-#define REFRESH_INTERVAL  5000      // refresh time, 5 SECONDS
-#define WRITE_INTERVAL 300000UL  // values send to serial port, 15 minutes (5 * 60 * 1000)
-#define KWH_2_WS 36000000 
-ModbusSensor wattmeter; // instance to collect data
-//variables to process and send values
-float power;
-float maxPower;
-float minPower;
-float energy;
-float lastEnergy;
-boolean firstData;
+#define MB_SERIAL_PORT &Serial1   // Arduino has only one serial port, Mega has 3 serial ports.
+// if use Serial 0, remember disconect Tx (pin0) when upload sketch, then re-conect
+#define MB_BAUDRATE       2400          // b 2400
+#define MB_BYTEFORMAT     SERIAL_8N2    // Prty n
+#define TxEnablePin       17
+
+#define ID_1  1                       // id 001  modbus id of the energy monitor
+#define REFRESH_INTERVAL  5000        // refresh time, 5 SECONDS
+#define WRITE_INTERVAL 20000UL        // values send to serial port, 1 minute ( 60 * 1000)
+#define KWH_2_WS 36000000
+
+// Direcciones registros de datos solo lectura. Valores tipo float.
+// Utilizar funcion 04 lectura, numero de registros 16-bits 2.
+
+#define VOL_ADR 0x0000    // VOLTAJE.
+#define CUR_ADR 0x0006    // CORRIENTE.
+#define POW_ADR 0x000C    // POTENCIA ACTIVA. 
+#define APO_ADR 0x0012    // Potencia Aparente.
+#define PFA_ADR 0x001E    // Factor de potencia.
+#define FRE_ADR 0x0046    // Frecuencia.
+#define PEN_ADR 0x0048    // ENERGIA IMPORTADA KWH
+#define REN_ADR 0x004A    // Energia exportada.
+#define TEN_ADR 0x0156    // Energia activa Total.
+#define TRE_ADR 0x0158    // Energia reactiva Total.
+
+// multiplication factor, store value as an integer
+#define VOL_FAC 10
+#define CUR_FAC 100
+#define POW_FAC 10
+#define PFA_FAC 100
+#define FRE_FAC 10
+#define ENE_FAC 100
+
+
+modbusMaster MBserial(MB_SERIAL_PORT, TxEnablePin);  // instance to collect data using Modbus protocol over RS485
+
+//variables to poll, process and send values
+modbusSensor volt(&MBserial, ID_1, VOL_ADR, CHANGE_TO_ZERO);
+modbusSensor curr(&MBserial, ID_1, CUR_ADR, CHANGE_TO_ZERO);
+modbusSensor pwr(&MBserial, ID_1, POW_ADR, CHANGE_TO_ZERO);
+modbusSensor enrg(&MBserial, ID_1, PEN_ADR, HOLD_VALUE);
+modbusSensor freq(&MBserial, ID_1, FRE_ADR, CHANGE_TO_ZERO);
+modbusSensor aPwr(&MBserial, ID_1, APO_ADR, CHANGE_TO_ZERO);
+modbusSensor pwrFact(&MBserial, ID_1, PFA_ADR, CHANGE_TO_ONE);
+
+uint16_t voltage, maxVoltage, minVoltage; // integer, factor x10
+uint16_t current, maxCurrent, minCurrent; // integer, factor x100
+uint16_t power, maxPower, minPower;       // integer, factor x10
+uint16_t lastEnergy, energy, avgPower;    // integer, factor x100
+uint16_t frequency, maxFreq, minFreq;     // integer, factor x100
+uint16_t aPower, maxApower, minApower;    // integer, factor x10
+uint16_t powerFactor, maxPF, minPF;       // integer, factor x100
 
 unsigned long previousMillis = 0;
 unsigned long currentMillis = 0;
-
+boolean       firstData;
 
 void setup() {
   SERIAL_BEGIN(9600);
-  wattmeter.begin(SERIAL_PORT, ID_SLAVE, REFRESH_INTERVAL);
-  SERIAL_PRINTLN("time(s), average power(W), max power(W), min power(W), energy(KWh)");
+  MBserial.begin(MB_BAUDRATE, MB_BYTEFORMAT, REFRESH_INTERVAL);
+  delay(95);
+  SERIAL_PRINTLN("time(s), maxVolt(V), minVolt(V), maxCurr(A) minCurr(A), maxPower(W), minPower(W), maxApPower(VA), minApPower(VA), maxFreq(Hz), minFreq(Hz), AvgPower (W), Energy(Kwh)");
 
   firstData = false;
   power = 0;
@@ -82,37 +90,75 @@ void setup() {
 
 void loop() {
   sei();
-  if (wattmeter.available()) {
-    power = wattmeter.read();
+  if (MBserial.available()) {
+    voltage = volt.read(VOL_FAC);
+    current = curr.read(CUR_FAC);
+    power = pwr.read(POW_FAC);
+    aPower = aPwr.read(POW_FAC);
+    frequency = freq.read(FRE_FAC);
+    energy = enrg.read(ENE_FAC);
+
     if (!firstData) {
+      if (maxVoltage < voltage) maxVoltage = voltage;
+      if (minVoltage > voltage) minVoltage = voltage;
+      if (maxCurrent < current) maxCurrent = current;
+      if (minCurrent > current) minCurrent = current;
+      if (maxPower < power) maxPower = power;
+      if (minPower > power) minPower = power;
+      if (maxApower < aPower) maxApower = aPower;
+      if (minApower > aPower) minApower = aPower;
+      if (maxFreq < frequency) maxFreq = frequency;
+      if (minFreq > frequency) minFreq = frequency;
       if (maxPower < power) maxPower = power;
       if (minPower > power) minPower = power;
     }
     else {
+      maxVoltage = voltage;
+      minVoltage = voltage;
+      maxCurrent = current;
+      minCurrent = current;
       maxPower = power;
       minPower = power;
+      maxApower = aPower;
+      minApower = aPower;
+      maxFreq = frequency;
+      minFreq = frequency;
       firstData = false;
     }
-    // backup energy, maxPower
   }
 
   currentMillis = millis();
   if (currentMillis - previousMillis >= WRITE_INTERVAL) {
     previousMillis = currentMillis;
-    energy = wattmeter.readAcum();
-    power = (energy - lastEnergy) * KWH_2_WS * 1000 / WRITE_INTERVAL; //average power KWh/s to W
+    avgPower = (energy - lastEnergy) * KWH_2_WS / (WRITE_INTERVAL / 1000); //average power KWh/s to W
     lastEnergy = energy;
     firstData = true;
 
     SERIAL_PRINT(currentMillis / 1000);
     SERIAL_PRINT(",");
-    SERIAL_PRINT(power);
+    SERIAL_PRINT((float)maxVoltage / VOL_FAC, 1);
     SERIAL_PRINT(",");
-    SERIAL_PRINT(maxPower);
+    SERIAL_PRINT((float)minVoltage / VOL_FAC, 1);
     SERIAL_PRINT(",");
-    SERIAL_PRINT(minPower);
+    SERIAL_PRINT((float)maxCurrent / CUR_FAC, 2);
     SERIAL_PRINT(",");
-    SERIAL_PRINTLN(energy);
+    SERIAL_PRINT((float)minCurrent / CUR_FAC, 2);
+    SERIAL_PRINT(",");
+    SERIAL_PRINT((float)maxPower / POW_FAC, 2);
+    SERIAL_PRINT(",");
+    SERIAL_PRINT((float)minPower / POW_FAC, 2);
+    SERIAL_PRINT(",");
+    SERIAL_PRINT((float)maxApower / POW_FAC, 2);
+    SERIAL_PRINT(",");
+    SERIAL_PRINT((float)minApower / POW_FAC, 2);
+    SERIAL_PRINT(",");
+    SERIAL_PRINT((float)maxFreq / FRE_FAC, 2);
+    SERIAL_PRINT(",");
+    SERIAL_PRINT((float)minFreq / FRE_FAC, 2);
+    SERIAL_PRINT(",");
+    SERIAL_PRINT((float)avgPower / ENE_FAC, 2);
+    SERIAL_PRINT(",");
+    SERIAL_PRINTLN((float)energy / ENE_FAC, 2);
 
   }
 }
--- Modbus-Energy-Monitor-Arduino/ModbusSensor.h
+++ Modbus-Energy-Monitor-Arduino/ModbusSensor.h
@@ -1,53 +1,112 @@
 /**********************************************************************
-* ModbusSensor class
-* A class to collect data from a Modbus energy monitor 
-*
-* version 0.3 ALPHA 21/09/2015
-* 
-* Author: Jaime García  @peninquen
-* License: Apache License Version 2.0.
-*
+  ModbusSensor.h
+  create ModbusSensor and ModbusMaster classes to process values from
+  a Eastron SMD120 and family.
+
+  version 0.3 BETA 22/12/2015
+
+  Author: Jaime García  @peninquen
+  License: Apache License Version 2.0.
+
 **********************************************************************/
 
 #ifndef ModbusSensor_h
 #define ModbusSensor_h
 
 #include "Arduino.h"
-#include "ModbusMaster.h"
+#define MAX_SENSORS 16
+// The maximum number of bytes in a modbus packet is 256 bytes.
+// The serial buffer limits this to 128 bytes.
+// We can reduce it to maximum data sending by a slave SMD120 9 bytes, SMD 630 is 85 bytes
+// Three phase meters are 3 values, 6 registers and 12 bytes, plus 5 frame bytes, total 17
+#define BUFFER_SIZE      32
+#define TIMEOUT          110  // time to fail a request 
+#define WAITING_INTERVAL 40   // time required by SDM120 to be prepared to receive a new request
 
-// Serial ports available
-// Arduino UNO
-// Arduino LEONARDO
-// Arduino MEGA 
+// What happens when _status is diferent to MB_VALID_DATA?
+#define CHANGE_TO_ZERO 0x00
+#define CHANGE_TO_ONE  0x01
+#define HOLD_VALUE     0xFF
 
-struct EnergyMonitor {
-  uint16_t voltage;
-  
+class modbusMaster;
+
+union dataFloat {
+  float f;
+  uint8_t array[4];
 };
 
-class ModbusSensor {
+//------------------------------------------------------------------------------
+class modbusSensor {
   public:
-    //constructor
-    ModbusSensor();
+    // Constructor
+    modbusSensor(modbusMaster *mbm, uint8_t id, uint16_t adr, uint8_t hold);
 
-    // Setup instance variables
-    void begin(int ModbusSerial, unsigned int idModbus, unsigned int interval);
+    // Constructor
+    //    modbusSensor(uint8_t id, uint16_t adr, uint8_t hold);
 
-    // check interval and update data
-    void refreshData();
-
-    // read v in defined units
+    // read value in defined units
     float read();
 
-    // reset
-    void reset();
+    // read value as a integer multiplied by factor
+    uint16_t read(uint16_t factor);
 
-    // is a new data available?
+    // get status of the value
+    inline uint8_t getStatus();
+
+    // write sensor value
+    inline void write(float value);
+
+    //  change status, return new status
+    inline uint8_t putStatus(uint8_t status);
+
+    // get pointer to _poll frame
+    inline uint8_t *getFramePtr();
+
+  private:
+    uint8_t     _frame[8];
+    dataFloat   _value;
+    uint8_t     _status;
+    uint8_t     _hold;
+};
+
+//------------------------------------------------------------------------------
+class modbusMaster {
+  public:
+    //constructor
+    modbusMaster(HardwareSerial *mbSerial, uint8_t TxEnPin);
+
+    // Connect a modbusSensor to modbusMaster array of queries
+    void connect(modbusSensor *mbSensor);
+
+    // Disconnect a modbusSensor to modbusMaster array of queries
+    void disconnect(modbusSensor *mbSensor);
+
+    // begin communication using ModBus protocol over RS485
+    void begin(uint16_t baudrate, uint8_t byteFormat, uint16_t pollInterval);
+
+    // end communication over serial port
+    void end();
+
+    // Finite State Machine core, process FSM and check if the array of sensors has been requested
     boolean available();
 
   private:
-    bool _flag;                        // true when data is processed, false when data is readed
-    EnergyMonitor energyMonitor;       // struct, buffer with last data received
+    inline void sendFrame();
+    inline void readBuffer(uint8_t frameSize);
+    uint8_t  _state;                    // Modbus FSM status (SENDING, RECEIVING, STANDBY, WAINTING_NEXT_POLL)
+    uint8_t  _TxEnablePin;              // pin to enable transmision in MAX485
+    uint8_t  _totalSensors;             // constant, max number of sensors to poll
+    uint16_t _pollInterval;             // constant, time between polling same data
+    uint32_t _T2_5;                     // time between characters in a frame, in microseconds
+    uint8_t  _buffer[BUFFER_SIZE];      // buffer to process rececived frame
+    //    uint8_t  _availableSensors;         // number of refreshed sensors, decrement when read, increment when write
+    //    uint16_t _availableSensorsFlag;     // array of flags to register new available sensor values
+    HardwareSerial  *_MBSerial;
+    modbusSensor    *_mbSensorsPtr[MAX_SENSORS]; // array of modbusSensor's pointers
+    modbusSensor    *_mbSensorPtr;
+    uint8_t         *_framePtr;
 };
+extern modbusMaster MBSerial;
 
 #endif
+
